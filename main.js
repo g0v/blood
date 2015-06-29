fd2ebf9ba817b78046d6c32cfb461bffaf4c3212
Yuren Ju <yurenju@gmail.com>
2015-06-30 00:17:34 +0800

    Fetch data from blood.org.tw

---
+++ main.js
@@ -0,0 +1,38 @@
+var jsdom = require('jsdom');
+require('shelljs/global');
+
+var status = {
+  'images/StorageIcon001.jpg': 'empty',
+  'images/StorageIcon002.jpg': 'medium',
+  'images/StorageIcon003.jpg': 'full'
+}
+
+jsdom.env(
+  'http://www.blood.org.tw/Internet/main/index.aspx',
+  ['http://code.jquery.com/jquery.js'],
+  function (errors, window) {
+    var $ = window.$;
+    var storages = $('.Storage').toArray();
+    var json = {};
+    storages.forEach(function(s) {
+      var name = $(s).find('#StorageHeader a').text();
+      json[name] = {name: name};
+      var types = ['StorageA', 'StorageB', 'StorageO', 'StorageAB']
+      types.forEach(function(type) {
+        var data = status[$(s).find('#' + type + ' img').attr('src')];
+        json[name][type] = data;
+      });
+    });
+    rm('-rf', 'out');
+    mkdir('-p', 'out');
+    cd('out');
+    exec('git init');
+    exec('git config user.name "Automatic Commit"');
+    exec('git config user.email "blood@g0v.tw"');
+    JSON.stringify(json, null, 2).to('blood.json');
+    exec('git add .');
+    exec('git commit -m "Automatic commit: ' + Date() + '"');
+    exec('git push --force "https://' + env['GH_TOKEN'] +
+         '@' + env['GH_REF'] + '" master:gh-pages');
+  }
+);
