64144b4ca1f076a97fe60fdaa2ac09e2e392552d
Yuren Ju <yurenju@gmail.com>
2015-07-01 08:50:12 +0800

    Support travis-ci

---
+++ utils/iron-upload.js
@@ -0,0 +1,27 @@
+var path = require('path');
+require('shelljs/global');
+
+const PROJECT_DIR = path.join(__dirname, '..');
+
+var iron = {
+  token: env['IRON_TOKEN'],
+  project_id: env['IRON_PROJECT_ID']
+};
+
+JSON.stringify(iron).to(PROJECT_DIR + '/iron.json');
+
+var worker = [
+  'runtime "node"',
+  'stack "node-0.10"',
+  'exec "main.js"',
+  'file "package.json"',
+  'build "npm config set strict-ssl false; npm install --production"',
+  'set_env "GH_TOKEN", "' + env['GH_TOKEN'] + '"',
+  'set_env "GH_REF", "github.com/g0v/blood.git"',
+  'remote'
+];
+
+worker.join('\n').to(PROJECT_DIR + '/blood.worker')
+
+
+exec('iron_worker upload blood', { silent: true });
