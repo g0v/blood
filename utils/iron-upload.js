941b7db73909581768b39981e123e99f7db1cff6
Yuren Ju <yurenju@gmail.com>
2015-07-11 00:41:20 +0800

    Fixed lint error on iron-upload.js

--- utils/iron-upload.js
+++ utils/iron-upload.js
@@ -1,11 +1,15 @@
+'use strict';
+
+/* global env, exec */
+
 var path = require('path');
 require('shelljs/global');
 
 const PROJECT_DIR = path.join(__dirname, '..');
 
 var iron = {
-  token: env['IRON_TOKEN'],
-  project_id: env['IRON_PROJECT_ID']
+  'token': env.IRON_TOKEN,
+  'project_id': env.IRON_PROJECT_ID
 };
 
 JSON.stringify(iron).to(PROJECT_DIR + '/iron.json');
@@ -16,12 +20,12 @@ var worker = [
   'exec "main.js"',
   'file "package.json"',
   'build "npm config set strict-ssl false; npm install --production"',
-  'set_env "GH_TOKEN", "' + env['GH_TOKEN'] + '"',
+  'set_env "GH_TOKEN", "' + env.GH_TOKEN + '"',
   'set_env "GH_REF", "github.com/g0v/blood.git"',
   'remote'
 ];
 
-worker.join('\n').to(PROJECT_DIR + '/blood.worker')
+worker.join('\n').to(PROJECT_DIR + '/blood.worker');
 
 
 exec('iron_worker upload blood', { silent: true });
