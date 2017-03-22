fda3877ffc9a9665ae6d3e9a4fc11a9098c12a6b
Yuren Ju (yurenju) <yurenju@gmail.com>
2017-03-22 12:22:45 +0800

    Revert "use node-4.1 and es6 syntax"

--- utils/iron-upload.js
+++ utils/iron-upload.js
@@ -1,30 +1,31 @@
+'use strict';
+
 /* global env, exec */
 
-const path = require('path');
+var path = require('path');
 require('shelljs/global');
-require('dotenv').config();
 
-const PROJECT_DIR = path.join(__dirname, '..');
+var PROJECT_DIR = path.join(__dirname, '..');
 
-const iron = {
-  token: process.env.IRON_TOKEN,
-  project_id: process.env.IRON_PROJECT_ID,
+var iron = {
+  'token': env.IRON_TOKEN,
+  'project_id': env.IRON_PROJECT_ID
 };
 
-JSON.stringify(iron).to(`${PROJECT_DIR}/iron.json`);
+JSON.stringify(iron).to(PROJECT_DIR + '/iron.json');
 
-const worker = [
+var worker = [
   'runtime "node"',
-  'stack "node-4.1"',
+  'stack "node-0.10"',
   'exec "main.js"',
   'file "package.json"',
   'build "npm config set strict-ssl false; npm install --production"',
-  `set_env "GH_TOKEN", "${process.env.GH_TOKEN}"`,
-  `set_env "GH_REF", "${process.env.GH_REF}"`,
-  'remote',
+  'set_env "GH_TOKEN", "' + env.GH_TOKEN + '"',
+  'set_env "GH_REF", "' + env.GH_REF + '"',
+  'remote'
 ];
 
-worker.join('\n').to(`${PROJECT_DIR}/blood.worker`);
+worker.join('\n').to(PROJECT_DIR + '/blood.worker');
 
 
 exec('iron_worker upload blood', { silent: true });
